86ce8e1413ae1b4b21f7cef643d6bdc3c132e21f
Esben Poulsen <esbenpouls@gmail.com>
2025-09-22 11:06:50 +0200

    Humble Beginnings

--- LabyrinthGameCS/MainWindow.xaml.cs
+++ LabyrinthGameCS/MainWindow.xaml.cs
@@ -16,9 +16,22 @@ namespace LabyrinthGameCS
     /// </summary>
     public partial class MainWindow : Window
     {
+        Player player = new Player("TestPlayer");
         public MainWindow()
         {
             InitializeComponent();
+        }
+    
+        void Move(object sender, RoutedEventArgs e)
+        {
+            string? input = ((Button)sender).Tag.ToString();
+            player.Move(input ?? "");
+            this.debug.Text = player.X.ToString() + "  " + player.Y.ToString();
+        }
+
+        void OpenInv(object sender, RoutedEventArgs e)
+        {
+            this.debug.Text = player.Name;
         }
     }
 }
---
+++ LabyrinthGameCS/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Player
+{
+	public string Name { get; set; }
+	public int X { get; set; }
+    public int Y { get; set; }
+    public Player(string name = "Default", int x = 0, int y = 0)
+	{
+		this.Name = name;
+		this.X = x; this.Y = y;
+	}
+
+	public bool Move(string dir)
+	{
+		switch (dir)
+		{
+			case "north":
+				this.Y += 1;
+				break;
+			case "south":
+				this.Y -= 1;
+				break;
+			case "east":
+				this.X += 1;
+				break;
+			case "west":
+				this.X -= 1;
+				break;
+			default:
+				return false;
+		}
+		return true;
+	}
+}
