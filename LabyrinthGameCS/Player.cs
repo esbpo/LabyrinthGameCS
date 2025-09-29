528e109b5b99ace4a23f8c64ab13a9bf84cc8bb4
Julian Reichkendler <julian.barenholdt@gmail.com>
2025-09-29 13:39:32 +0200

    hej

--- LabyrinthGameCS/Player.cs
+++ LabyrinthGameCS/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Media;
 
 public class Player
 {
@@ -33,3 +34,41 @@ public class Player
 		return true;
 	}
 }
+using System;
+
+public class labrinth
+{
+    public class RectangleForm : Form
+    {
+        public RectangleForm()
+        {
+            this.Size = new Size(400, 300);
+            this.Paint += new PaintEventHandler(DrawRectangle);
+        }
+
+        private void DrawRectangle(object sender, PaintEventArgs e)
+        {
+            //placement: x=50, y=40, width=200, height=100
+            Rectangle rect = new Rectangle(50, 10, 200, 100);
+
+            // Use a brush to fill rectangle
+            using (SolidBrush brush = new SolidBrush(Color.#00ffbc))
+            {
+                e.Graphics.FillRectangle(brush, rect);
+            }
+
+            // Optional: draw outline
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                e.Graphics.DrawRectangle(pen, rect);
+            }
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new RectangleForm());
+        }
+    }
+}
+
---
+++ LabyrinthGameCS/labrinth.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LabyrinthGameCS
+{
+    public class RectangleForm : Form
+    {
+        public RectangleForm()
+        {
+            this.Text = "Green Rectangle Example";
+            this.Size = new Size(400, 300);
+            this.Paint += new PaintEventHandler(DrawRectangle);
+        }
+
+        private void DrawRectangle(object sender, PaintEventArgs e)
+        {
+            // Placering: x=50, y=40, width=200, height=100
+            Rectangle rect = new Rectangle(50, 40, 200, 100);
+
+            // Grøn fyldfarve (hex: #00ffbc)
+            using (SolidBrush brush = new SolidBrush(ColorTranslator.FromHtml("#00ffbc")))
+            {
+                e.Graphics.FillRectangle(brush, rect);
+            }
+
+            // Valgfri: sort kant
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                e.Graphics.DrawRectangle(pen, rect);
+            }
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new RectangleForm());
+        }
+    }
+}
